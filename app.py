b0a03a7f4678415b528fad9a08d11f714fcd5169
james-koduah <jameskoduah171@gmail.com>
2025-01-23 20:25:16 +0000

    Build 1 of Visual interface

--- app.py
+++ app.py
@@ -1,235 +1,156 @@
-import re
-import sys
-import piexif
-from PIL import Image
-import os
 from datetime import datetime
+import os
+import sys
 import time
-from dotenv import load_dotenv
-load_dotenv()
+from flask import Flask, jsonify, render_template, request
+from flask_socketio import SocketIO, emit
+from image_exif import sort_all_filenames_in_folder_numerically, make_sure_all_images_are_present, add_exif, filter_broken_brackets
+import webbrowser
+import threading
+import socket
 
+app = Flask(__name__)
 
-IMAGES_FOLDER = os.getenv("IMAGES_FOLDER")
-OUTPUT_FOLDER = os.getenv("OUTPUT_FOLDER")
-OUTPUT_LOG_FILE = os.getenv("OUTPUT_LOG_FILE")
-PREVIEW_MODE = os.getenv("PREVIEW_MODE", 'true').lower() in ("true", "1", "yes")
-SUPRESS_ERRORS = os.getenv("SUPRESS_ERRORS", 'False').lower() in ("true", "1", "yes")
-CAMERA_MAKE = os.getenv("CAMERA_MAKE", "Unknown Camera Make")
-CAMERA_MODEL = os.getenv("CAMERA_MODEL", "Unknown Camera Model")
-WHITEBALANCE = os.getenv("WHITEBALANCE", 0)
-FOCAL_LENGTH = os.getenv("FOCAL_LENGTH", 50)
-ISO_SPEED = os.getenv("ISO_SPEED", 100)
-APERTURE = os.getenv("APERTURE", 28)
+app.config['SECRET_KEY'] = 'verysecretlol'
+socketio = SocketIO(app)
 
-FRAME_ORDER = os.getenv("FRAME_ORDER", "under,over,normal")
-FRAME_ORDER = FRAME_ORDER.split(',')
-VALID_EXTENSIONS = os.getenv("VALID_EXTENSIONS", ".jpg,.png,.tiff,.tif")
-VALID_EXTENSIONS = VALID_EXTENSIONS.split(',')
+keep_running = True
 
-UNDER_EXPOSURE_TIME = os.getenv("UNDER_EXPOSURE_TIME", 500)
-UNDER_EXPOSURE_BIAS = os.getenv("UNDER_EXPOSURE_BIAS", -2)
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-NORMAL_EXPOSURE_TIME = os.getenv("NORMAL_EXPOSURE_TIME", 100)
-NORMAL_EXPOSURE_BIAS = os.getenv("NORMAL_EXPOSURE_BIAS", 0)
-
-OVER_EXPOSURE_TIME = os.getenv("OVER_EXPOSURE_TIME", 50)
-OVER_EXPOSURE_BIAS = os.getenv("OVER_EXPOSURE_BIAS", 2)
-
-
-
-def convert_to_float(name, value):
+@app.route('/begin_processing', methods=['post'])
+def begin_processing():
+    data = request.get_json()
     try:
-        v = float(value)
-        return v
-    except Exception:
-        print(f'Error. {name} must be a number. {value} is not a correct value')
-
-def convert_to_int(name, value):
-    try:
-        v = int(value)
-        return v
-    except Exception:
-        print(f'Error. {name} must be a number. {value} is not a correct value')
-
-
-
-UNDER_EXPOSURE_TIME = convert_to_int("UNDER_EXPOSURE_TIME", UNDER_EXPOSURE_TIME)
-UNDER_EXPOSURE_BIAS = convert_to_int("UNDER_EXPOSURE_BIAS", UNDER_EXPOSURE_BIAS)
-NORMAL_EXPOSURE_TIME = convert_to_int("NORMAL_EXPOSURE_TIME", NORMAL_EXPOSURE_TIME)
-NORMAL_EXPOSURE_BIAS = convert_to_int("NORMAL_EXPOSURE_BIAS", NORMAL_EXPOSURE_BIAS)
-OVER_EXPOSURE_TIME = convert_to_int("OVER_EXPOSURE_TIME", OVER_EXPOSURE_TIME)
-OVER_EXPOSURE_BIAS = convert_to_int("OVER_EXPOSURE_BIAS", OVER_EXPOSURE_BIAS)
-WHITEBALANCE = convert_to_int("WHITEBALANCE", WHITEBALANCE)
-FOCAL_LENGTH = convert_to_int("FOCAL_LENGTH", FOCAL_LENGTH)
-ISO_SPEED = convert_to_int("ISO_SPEED", ISO_SPEED)
-APERTURE = convert_to_int("APERTURE", APERTURE)
-
-
-def preview_mode():
-    print('\nHere are the metadata changes to be added to the images:')
-    print(f'Camera Make: {CAMERA_MAKE}\nCamera Model: {CAMERA_MODEL}')
-    print(f"White Balance: {WHITEBALANCE}\nFocal Length: {FOCAL_LENGTH}mm")
-    print(f"ISO Speed Ratings: {ISO_SPEED}\nAperture/FNumber: {APERTURE}/10")
-
-    print('--------- UNDER FRAME ----------')
-    print(f'Exposure Time: 1/{UNDER_EXPOSURE_TIME}\nExposure Bias: {UNDER_EXPOSURE_BIAS} EV')
-
-    print('--------- NORMAL FRAME ----------')
-    print(f'Exposure Time: 1/{NORMAL_EXPOSURE_TIME}\nExposure Bias: {NORMAL_EXPOSURE_BIAS} EV')
-
-    print('--------- OVER FRAME ----------')
-    print(f'Exposure Time: 1/{OVER_EXPOSURE_TIME}\nExposure Bias: {OVER_EXPOSURE_BIAS} EV')
+        filenames = sort_all_filenames_in_folder_numerically(data.get('folder_path'))
+        if len(filenames) == 0:
+            return jsonify({'status': False, 'problem': 'empty_folder'})
+    except:
+        return jsonify({'status': False, 'problem': 'bad_path'})
     
-    print("Are you okay with these changes?")
-    answer = input("Continue processing? (yes/no): ")
-    if answer.lower() != 'yes' and answer.lower() != 'y':
-        print('Aborting...')
-        return False
-    return True
+    check = make_sure_all_images_are_present(filenames)
+    if check:
+        return jsonify({'status': False, 'problem': 'missing_files', 'data': check})
     
+    process_files(data, filenames)
+    return jsonify(filenames)
 
-def validate_config_values():
-    # Validate Config Values
-    if not IMAGES_FOLDER:
-        print("Error. Missing Config Value: IMAGES_FOLDER")
-        print("Please Specify the folder containing the images Images in the .env config file")
-        return False
-    
-    if not OUTPUT_FOLDER:
-        print("Caution: No output folder specified. The original image will be edited.")
-        answer = input("Continue processing? (yes/no): ")
-        if answer.lower() != 'yes' and answer.lower() != 'y':
-            print('Aborting...')
-            return False
-    return True
+@app.route('/continue_processing', methods=['post'])
+def continue_processing():
+    data = request.get_json()
+    filenames = sort_all_filenames_in_folder_numerically(data.get('folder_path'))
+    process_files(data, filenames)
+    return jsonify({})
 
+@app.route('/skip_broken', methods=['post'])
+def skip_broken():
+    data = request.get_json()
+    filenames = sort_all_filenames_in_folder_numerically(data.get('folder_path'))
+    filtered_brackets = filter_broken_brackets(filenames, len(data.get('frame_sequence')))
+    process_files(data, filtered_brackets)
+    return jsonify({})
 
-hdr_metadata = {
-    "under": {
-        "ExposureTime": (1, UNDER_EXPOSURE_TIME),       # Exposure time: 1/200s
-        "FNumber": (APERTURE, 10),           # Aperture: f/2.8
-        "ISOSpeedRatings": ISO_SPEED,        # ISO 100
-        "ExposureBiasValue": (UNDER_EXPOSURE_BIAS, 1),  # Exposure bias: -2 EV
-    },
-    "normal": {
-        "ExposureTime": (1, NORMAL_EXPOSURE_TIME),      # Exposure time: 1/100s
-        "FNumber": (APERTURE, 10),           # Aperture: f/2.8
-        "ISOSpeedRatings": ISO_SPEED,        # ISO 100
-        "ExposureBiasValue": (NORMAL_EXPOSURE_BIAS, 1),   # Exposure bias: 0 EV
-    },
-    "over": {
-        "ExposureTime": (1, OVER_EXPOSURE_TIME),       # Exposure time: 1/50s
-        "FNumber": (APERTURE, 10),           # Aperture: f/2.8
-        "ISOSpeedRatings": ISO_SPEED,        # ISO 100
-        "ExposureBiasValue": (OVER_EXPOSURE_BIAS, 1),   # Exposure bias: +2 EV
-    },
-}
+# def process_files(data, filenames):
+#     global stop_event
+#     stop_event.clear()
+#     thread = threading.Thread(target=process_files_, args=(data, filenames))
+#     thread.start()
 
+def process_files(data, filenames):
 
-def add_exif(image_path, type, date):
-    metadata = hdr_metadata.get(type)
-    if not metadata:
-        return
-    
-    # Build EXIF dictionary
-    exif_dict = {
-        "Exif": {
-            piexif.ExifIFD.ExposureTime: metadata["ExposureTime"],
-            piexif.ExifIFD.FNumber: metadata["FNumber"],
-            piexif.ExifIFD.ISOSpeedRatings: metadata["ISOSpeedRatings"],
-            piexif.ExifIFD.ExposureBiasValue: metadata["ExposureBiasValue"],
-            piexif.ExifIFD.FocalLength: (FOCAL_LENGTH, 1),
-            piexif.ExifIFD.WhiteBalance: WHITEBALANCE, 
-        },
-        "0th": {
-            piexif.ImageIFD.Make: CAMERA_MAKE,
-            piexif.ImageIFD.Model: CAMERA_MODEL,  
-            piexif.ImageIFD.DateTime: date,
-        },
-    }
-
-    # Convert EXIF dictionary to bytes
-    exif_bytes = piexif.dump(exif_dict)
-
-    # Open the image and save with updated EXIF data
-    image = Image.open(f"{IMAGES_FOLDER}/{image_path}")
-    if OUTPUT_FOLDER:
-        try:
-            image.save(f"{OUTPUT_FOLDER}/{image_path}", exif=exif_bytes)
-        except Exception as e:
-            print("Output Folder does not exist. Please create it.")
-            sys.exit()
-    else:
-        image.save(f"{IMAGES_FOLDER}/{image_path}", exif=exif_bytes)
-
-
-filename_sequence = []
-
-def extract_leading_number(filename):
-    match = re.match(r"(\d+)", filename)  # Match leading numbers
-    result = int(match.group(1)) if match else float('inf')
-    filename_sequence.append(result)
-    return result
-
-
-def sort_all_filenames_in_folder_numerically():
-    image_filenames = [
-        filename for filename in os.listdir(IMAGES_FOLDER)
-        if os.path.isfile(os.path.join(IMAGES_FOLDER, filename)) and any(filename.lower().endswith(ext) for ext in VALID_EXTENSIONS)
-    ]
-    return sorted(image_filenames, key=extract_leading_number)
-
-def make_sure_all_images_are_present(arr):
-    lowest = min(arr)
-    highest = max(arr)
-    expected_numbers = set(range(lowest, highest + 1))
-    missing_numbers = expected_numbers - set(arr)
-    if missing_numbers:
-        print(f"MISSING FRAME(s) (starts with): {sorted(missing_numbers)}")
-        print('Error: Frame(s) missing in the sequence. Please check your IMAGE FOLDER and make sure all frames are present in their correct order.')
-        input('Press any key to continue: ')
-        return False
-    return True
-
-def main():
-    print(f"\nProcessing images from Folder: {IMAGES_FOLDER}")
-    print(f"Image frame pattern is: {FRAME_ORDER}\n")
     frame_number = 0
     bracket_date = None
+    folder_path = data.get('folder_path')
+    output_path = data.get('output_path')
+    frame_sequence = data.get('frame_sequence')
+    metadata = {
+        'camera_make': data.get('camera_make'),
+        'camera_model': data.get('camera_model'),
+        'white_balance': int(data.get('white_balance')),
+        'focal_length': int(data.get('focal_length')),
+        'iso_speed': int(data.get('iso_speed')),
+        'aperture': int(data.get('aperture'))
+    }
+    frame_data = {
+        'under': {
+            'exposure_time': int(data.get('under_exposure_time')),
+            'exposure_bias': int(data.get('under_exposure_bias')),
+        },
+        'mild-under': {
+            'exposure_time': int(data.get('mild_under_exposure_time')) if data.get('mild_under_exposure_time') else None,
+            'exposure_bias': int(data.get('mild_under_exposure_bias')) if data.get('mild_under_exposure_bias') else None,
+        },
+        'normal': {
+            'exposure_time': int(data.get('normal_exposure_time')),
+            'exposure_bias': int(data.get('normal_exposure_bias')),
+        },
+        'mild-over': {
+            'exposure_time': int(data.get('mild_over_exposure_time')) if data.get('mild_over_exposure_bias') else None,
+            'exposure_bias': int(data.get('mild_over_exposure_bias')) if data.get('mild_over_exposure_bias') else None,
+        },
+        'over': {
+            'exposure_time': int(data.get('over_exposure_time')),
+            'exposure_bias': int(data.get('over_exposure_bias'),)
+        }
+    }
+    global keep_running
+    keep_running = True
+    for i in range(0, len(filenames)):
+        if not keep_running:
+            break
+        filename = filenames[i]
+        if frame_number == 0:
+            socketio.emit('processing', [filenames[i+j] for j in range(0, len(frame_sequence))])
+            time.sleep(2)
+            bracket_date = datetime.now()
+            bracket_date = bracket_date.strftime("%Y:%m:%d %H:%M:%S")
+        current_frame_type = frame_sequence[frame_number]
+        add_exif(folder_path, filename, metadata, frame_data.get(current_frame_type), bracket_date, output_path)
+        frame_number += 1
+        if frame_number >= len(frame_sequence):
+            frame_number = 0
 
-    sorted_filenames = sort_all_filenames_in_folder_numerically()
-    proceed = make_sure_all_images_are_present(filename_sequence)
-    if not proceed and not SUPRESS_ERRORS:
-        print("To suppress this error and proceed, set SUPRESS_ERRORS in the config file to true\n")
-        input("Press Any key to Abort: ")
-        return
-    
-    for i in range(0, len(sorted_filenames)):
-        filename = sorted_filenames[i]
-        if any(filename.lower().endswith(ext) for ext in VALID_EXTENSIONS):
-            if frame_number == 0:
-                print(f"Processing frame group ({sorted_filenames[i], sorted_filenames[i+1], sorted_filenames[i+2]})")
-                time.sleep(2)
-                bracket_date = datetime.now()
-                bracket_date = bracket_date.strftime("%Y:%m:%d %H:%M:%S")
-            
-            add_exif(filename, FRAME_ORDER[frame_number], bracket_date)
-            frame_number += 1
-            if frame_number > 2:
-                frame_number = 0
+def shutdown_server():
+    """Gracefully shuts down the Flask server."""
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        os._exit(0)
+    func()
 
-def run():
-            
-    proceed = validate_config_values()
-    if proceed:
-        if PREVIEW_MODE:
-            proceed = preview_mode()
-            if not proceed:
-                return
-        main()
-    else:
-        hh = input("Press any key to abort")
-        return
+@socketio.on('stop_processing')
+def stop_process():
+    """Stops the long-running process."""
+    global keep_running
+    keep_running = False
+    emit('stopped_process')
+
+# @socketio.on('disconnect')
+# def handle_disconnect(data):
+#     shutdown_server()
+
+
+def open_browser(port):
+    # Open the browser to the specified port
+    webbrowser.open(f"http://127.0.0.1:{port}")
+
+def find_free_port(default_port=5000):
+    # Try to bind to the default port; if unavailable, find another free port
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    try:
+        sock.bind(("127.0.0.1", default_port))
+        port = sock.getsockname()[1]
+    except OSError:
+        # If binding fails, find an available port
+        sock.bind(("127.0.0.1", 0))
+        port = sock.getsockname()[1]
+    finally:
+        sock.close()
+    return port
 
 if __name__ == '__main__':
-    run()
+    port = find_free_port()  # Get an available port
+    threading.Thread(target=open_browser, args=(port,)).start()
+    socketio.run(app)
---
+++ image_exif.py
@@ -0,0 +1,100 @@
+import re
+import piexif
+from PIL import Image
+import os
+
+
+def add_exif(folder_path, image_path, metadata, frame_data, date, output_folder=False):
+    
+    # Build EXIF dictionary
+    exif_dict = {
+        "Exif": {
+            piexif.ExifIFD.ExposureTime: (1, frame_data.get('exposure_time')),
+            piexif.ExifIFD.FNumber: (metadata.get('aperture'), 10),
+            piexif.ExifIFD.ISOSpeedRatings: metadata.get('iso_speed'),
+            piexif.ExifIFD.ExposureBiasValue: (frame_data.get('exposure_bias'), 1),
+            piexif.ExifIFD.FocalLength: (metadata.get('focal_length'), 1),
+            piexif.ExifIFD.WhiteBalance: metadata.get('white_balance'), 
+        },
+        "0th": {
+            piexif.ImageIFD.Make: metadata.get('camera_make'),
+            piexif.ImageIFD.Model: metadata.get('camera_model'),  
+            piexif.ImageIFD.DateTime: date,
+        },
+    }
+
+    # Convert EXIF dictionary to bytes
+    exif_bytes = piexif.dump(exif_dict)
+
+    # Open the image and save with updated EXIF data
+    image = Image.open(f"{folder_path}/{image_path}")
+    if output_folder:
+        try:
+            image.save(f"{output_folder}/{image_path}", exif=exif_bytes)
+        except Exception as e:
+            pass
+    else:
+        image.save(f"{folder_path}/{image_path}", exif=exif_bytes)
+
+
+# filename_sequence = []
+
+def extract_leading_number(filename):
+    match = re.match(r"(\d+)", filename)  # Match leading numbers
+    result = int(match.group(1)) if match else float('inf')
+    return result
+
+
+def sort_all_filenames_in_folder_numerically(images_folder):
+    image_filenames = [
+        filename for filename in os.listdir(images_folder)
+        if os.path.isfile(os.path.join(images_folder, filename)) and any(filename.lower().endswith(ext) for ext in ['.jpg', '.tif', '.tiff', '.png'])
+    ]
+    return sorted(image_filenames, key=extract_leading_number)
+
+def filter_broken_brackets(filenames, group_size):
+    """
+    Filters a list of numerically sorted filenames, removing groups where a member is missing.
+
+    Args:
+      filenames: A list of filenames, assumed to be numerically sorted.
+      group_size: The size of the groups to consider.
+
+    Returns:
+      A list of filenames with missing groups removed.
+    """
+
+    filtered_filenames = []
+    start = 0
+    while start < len(filenames):
+        bracket_start = extract_leading_number(filenames[start])
+        bracket_end = bracket_start + group_size - 1 # the number the current bracket should end at
+        file_at_end = extract_leading_number(filenames[start + group_size -1])
+        if file_at_end == bracket_end:
+            for i in range(start, (start+group_size)):
+                filtered_filenames.append(filenames[i])
+        else:
+            next_supposed_file = bracket_start + group_size
+            found = False
+            while not found and next_supposed_file < len(filenames):
+                for i in range(start, len(filenames)):
+                    if extract_leading_number(filenames[i]) == next_supposed_file:
+                        start = i
+                        found = True
+                        break
+                next_supposed_file = next_supposed_file + group_size
+            continue
+        start = start + group_size
+    return filtered_filenames
+
+def make_sure_all_images_are_present(raw_arr):
+    arr = []
+    for i in raw_arr:
+        arr.append(extract_leading_number(i))
+    lowest = min(arr)
+    highest = max(arr)
+    expected_numbers = set(range(lowest, highest + 1))
+    missing_numbers = expected_numbers - set(arr)
+    if missing_numbers:
+        return list(missing_numbers)
+    return False
